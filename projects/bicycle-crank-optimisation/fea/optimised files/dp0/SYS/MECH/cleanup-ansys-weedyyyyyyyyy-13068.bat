18bf287b0de1c9261254376caec829f281a6afe4
billytoothpunch <81867313+billytoothpunch@users.noreply.github.com>
2026-07-19 21:12:03 +0100

    Add files via upload

---
+++ projects/bicycle-crank-optimisation/fea/optimised files/dp0/SYS/MECH/cleanup-ansys-weedyyyyyyyyy-13068.bat
@@ -0,0 +1,8 @@
+@echo off
+set LOCALHOST=%COMPUTERNAME%
+if /i "%LOCALHOST%"=="weedyyyyyyyyy" (taskkill /f /pid 22388)
+if /i "%LOCALHOST%"=="weedyyyyyyyyy" (taskkill /f /pid 24872)
+if /i "%LOCALHOST%"=="weedyyyyyyyyy" (taskkill /f /pid 25096)
+if /i "%LOCALHOST%"=="weedyyyyyyyyy" (taskkill /f /pid 13068)
+
+del /F cleanup-ansys-weedyyyyyyyyy-13068.bat
